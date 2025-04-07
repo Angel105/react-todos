bc1577d77e7299d32174c557c9ba3cc9de193985
Borzoi <siteandstock@gmail.com>
2025-04-07 16:48:02 +0200

    Convert NewTodoForm plain function to arrow function returning React Functional Component: React.FC<{addTodo: Function}>

--- src/components/NewTodoForm.tsx
+++ src/components/NewTodoForm.tsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react'
 
-function NewTodoForm(propz: {addTodo: Function}) {
+export const NewTodoForm: React.FC<{addTodo: Function}> = (propz) => {
 
     const [description, setDescription] = useState('');
     const [assigned, setAssigned] = useState('');
@@ -45,5 +45,3 @@ function NewTodoForm(propz: {addTodo: Function}) {
         </div>
     )
 }
-
-export default NewTodoForm;
